9e4608abd91a978b5c5bf92c483dee2146af7b28
booringreader <157519312+booringreader@users.noreply.github.com>
2024-10-09 17:47:17 +0530

    parcel func

--- App.js
+++ App.js
@@ -1,6 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+const parent = React.createElement("div",{id: "parent"},
+    React.createElement("div", {id: "child"}, [
+        React.createElement("h1", {}, "I am a heading"),
+        React.createElement("h2", {}, "I am a smaller heading")
+    ]),
+    React.createElement("div", {id: "child"}, [
+        React.createElement("h1", {}, "I am a heading"),
+        React.createElement("h2", {}, "I am a much smaller heading")
+    ])
+);
+
 const heading = React.createElement("h1",{id: "heading"}, "Hello world")
 
 const root = ReactDOM.createRoot(document.getElementById("root")); // creates a root for the react to execute from (passing in root id of the div)
 
 root.render(heading);
+root.render(parent);
 
