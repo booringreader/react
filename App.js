66e35a20e970459962a9b0154e112f83d3e9e612
booringreader <157519312+booringreader@users.noreply.github.com>
2024-10-10 11:21:07 +0530

    component composition

--- App.js
+++ App.js
@@ -1,9 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const jsxHeading = <h1 id="heading"> jsx created heading</h1>
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// jsx ---Babel--> "React.createElement()" -> ReactElement / JS object -> rendered as HTML (DOM) element
+// React functional component
+const Title = function() { // written as normal function instead of an arrow function
+    return (
+        <h1> React Tut</h1>
+    );
+};
+const HeadingComponent = () => (
+        // component composition
+        <div className="container">
+            <h1>functional component</h1> {/* returns <h1></h1> */}
+            <Title />
+        </div>
+    );
+//const HeadingComponent = () => (
+//    // component composition
+//    <div className="container">
+//        <h1>functional component</h1> {/* returns <h1></h1> */}
+//        <Title />
+//    </div>
+//);
 
-root.render(jsxHeading);
+// React Element
+const headingElement = (
+    <h1>react element</h1>
+);
+root.render(<HeadingComponent />);
+// root.render(<Title />);
+// root.render(headingElement);
