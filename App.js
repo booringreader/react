25d62f6f6d1b9055268fcf1539bcd7d6dacef7ba
booringreader <157519312+booringreader@users.noreply.github.com>
2024-10-12 11:08:44 +0530

    inline style demo

--- App.js
+++ App.js
@@ -1,35 +1,66 @@
+// TODO - Header, Body, Footer
+/* 
+*   Header
+    - Logo
+    - Navbar Links
+*   Body
+    - Search Bar
+    - Restaurant cards
+* Footer
+    - Copyright
+    - Social links
+*/
+// ? component(div(div(div)))
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// React functional component
-const Title = function() { // written as normal function instead of an arrow function
+const Header = () => {
     return (
-        <h1> React Tut</h1>
-    );
-};
-const HeadingComponent = () => (
-        // component composition
-        <div className="container">
-            <h1>functional component</h1> {/* returns <h1></h1> */}
-            <Title />
-            {<Title()>}
-            <Title> </Title>
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://png.pngtree.com/png-vector/20220623/ourmid/pngtree-food-logo-png-image_5297921.png"></img>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
         </div>
-    );
-//const HeadingComponent = () => (
-//    // component composition
-//    <div className="container">
-//        <h1>functional component</h1> {/* returns <h1></h1> */}
-//        <Title />
-//    </div>
-//);
+    )
+}
 
-// React Element
-const headingElement = (
-    <h1>react element</h1>
-);
-root.render(<HeadingComponent />);
-// root.render(<Title />);
-// root.render(headingElement);
+const stylecard = { // ? js object
+    backgroundColor: "yellow"
+}
+const RestaurantCard = () => {
+    return (
+        <div className="res-card" style={stylecard}>
+            <h3>The Green Bean</h3>
+        </div>
+    )
+}
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">
+                search
+            </div>
+            <div className="res-container">
+                <RestaurantCard />
+            </div>
+        </div>
+    )
+}
+
+const AppLayout = () => {
+    return <div className="app">
+        <Header />
+        <Body />
+    </div>
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout/>);
