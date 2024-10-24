7000f35f14371cad7ec7a3f5e67e921e2077f01a
booringreader <157519312+booringreader@users.noreply.github.com>
2024-10-24 21:22:30 +0530

    children routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,43 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header.js";
 import RestaurantCard from "./components/RestaurantCard.js"
 import Body from "./components/Body.js";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import About from "./components/About.js";
+import Contact from "./components/Contact.js";
+import Error from "./components/Error.js";
+import RestaurantMenu from "./components/RestaurantMenu.js";
 
 const AppLayout = () => {
     return <div className="app">
         <Header />
-        <Body />
+        <Outlet />
     </div>
 }
+
+const appRouter = createBrowserRouter([
+    {
+        path: "/",
+        element: <AppLayout />,
+        children: [
+            {
+                path: "/",
+                element: <Body />,
+            },
+            {
+                path: "/about",
+                element: <About />,
+            },
+            {
+                path: "/contact",
+                element: <Contact />,
+            },
+            {
+                path: "/restaurants/:resId", // accessed via useParams() hook
+                element: <RestaurantMenu /> // ':' tells the react-router-dom that the following is a dynamic entity
+            }
+        ],
+        errorElement: <Error />,
+    },
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,7 @@
 import RestaurantCard from "./RestaurantCard";
 import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
+import {Link} from "react-router-dom"
 
 const Body = () => {
     // ? state variable declaration
@@ -18,14 +19,16 @@ const Body = () => {
         const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.65200&lng=77.16630&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
 
         const json = await data.json();
-        console.log(json);
+        
         setlistRest(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants) // optional chaining
         setsearchRest(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
     };
 
     // ? contents of the body component
-    return listRest.length === 0 ? (<Shimmer />) : (<div className="body">
+    return listRest.length === 0 ? (<Shimmer />) : (
+    <div className="body">
         <div className="filter">
+            
             <div className="search">
                 {/* ? search bar */}
                 <input type="text" className="search-box" value={searchText} onChange={(e) =>
@@ -36,18 +39,23 @@ const Body = () => {
                     const filteredRes = listRest.filter((res) => res.info.name.toLowerCase().includes(searchText.toLowerCase())
                     );
                     setsearchRest(filteredRes);
-                }}>Search</button>
+                }}>Search
+                </button>
+
             </div>
                 
                 {/*"top restaurant" filter button */}
             <button className="filter-btn" onClick={() => {
                 const filteredList = listRest.filter((res) => res.info.avgRating > 4.3);
                 setsearchRest(filteredList); // ? update listRest with filteredList
-            }}>Top Restaurants</button>
+            }}>Top Restaurants
+            </button>
+
         </div>
+        
         <div className="res-container">
             {searchRest.map(restaurant => (
-                <RestaurantCard key={restaurant.info.id} resData={restaurant} />))}
+                <Link key={restaurant.info.id} to={"/restaurants/"+restaurant.info.id}><RestaurantCard resData={restaurant} /></Link>))}
         </div>
     </div >
     );
